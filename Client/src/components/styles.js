1d490d077f043f7c5b024020d33f6accaa3247a7
HiUlisesOk <uesquivel95@gmail.com>
2023-02-19 15:49:58 -0300

    :sparkles: Navbar and Logo created

--- Client/src/components/styles.js
+++ Client/src/components/styles.js
@@ -13,8 +13,8 @@ export const FlexContainer = styled.div`
 export const Title = styled.h1`
 font-style: normal;
 font-weight: 700;
-font-size: 1.5rem;
-line-height: 1.5rem;
+font-size:  ${props => props.size ? props.size : "1.5rem"};
+line-height:${props => props.lh ? props.lh : "1.5rem"};
 letter-spacing:1.5%;
 margin: ${props => props.margin ? props.margin : "12px"};
 text-align: ${props => props.align ? props.align : "left"};
@@ -22,14 +22,14 @@ text-align: ${props => props.align ? props.align : "left"};
 
 export const SubTitle = styled(Title)`
 font-weight: 400;
-font-size: 2rem;
-line-height: 1.125rem;
+font-size:  ${props => props.size ? props.size : "1.5rem"};
+line-height:${props => props.lh ? props.lh : "1.125rem"};
 `;
 
 export const Text = styled(Title)`
 font-weight: 400;
-font-size: 0.8rem;
-line-height: 0.8rem;
+font-size:  ${props => props.size ? props.size : "0.5rem"};
+line-height:${props => props.lh ? props.lh : "0.8rem"};
 `;
 
 export const TextPrimary = styled.span`
