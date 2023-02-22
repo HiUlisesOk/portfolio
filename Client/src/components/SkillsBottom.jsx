642fb12e0ec692231e25ce05169fe16fde0705f9
HiUlisesOk <uesquivel95@gmail.com>
2023-02-21 23:41:36 -0300

    :art: Finished tech skills

--- Client/src/components/SkillsBottom.jsx
+++ Client/src/components/SkillsBottom.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import { FlexContainer } from './styles'
+import MediumTech from '../images/Assets/MediumTech.png'
 const SkillsBottom = () => {
 	return (
-		<div>SkillsBottom</div>
+		<FlexContainer mt="12px"><img src={MediumTech} /></FlexContainer>
 	)
 }
 
