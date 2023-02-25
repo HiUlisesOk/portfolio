944ca48158802b7a9b3f79ca303f3dd2fec3ca65
HiUlisesOk <uesquivel95@gmail.com>
2023-02-25 14:32:38 -0300

    :art: Cards project styles updated

--- Client/src/pages/RecentProjects.jsx
+++ Client/src/pages/RecentProjects.jsx
@@ -7,11 +7,11 @@ import findahome from '../images/Assets/cards/Rectangle 11 (2).png'
 import data from "../json/projects-data.json"
 const RecentProjects = () => {
 	return (
-		<FlexContainer direction="column"><SubTitle>Latest Projects</SubTitle>
-			<FlexContainer>
-				<Card image={RickAndMorty} title={data[0].title} subtitle={data[0].subtitle} text={data[0].subtitle} link={data[0].link} />
-				<Card image={foods} title={data[1].title} subtitle={data[1].subtitle} text={data[1].subtitle} link={data[1].link} />
-				<Card image={findahome} title={data[2].title} subtitle={data[2].subtitle} text={data[2].subtitle} link={data[2].link} />
+		<FlexContainer items="stretch" direction="column"><SubTitle>Latest Projects</SubTitle>
+			<FlexContainer items="stretch">
+				<Card image={RickAndMorty} title={data[0].title} techFront={data[0].techFront} techBack={data[0].techBack} text={data[0].subtitle} link={data[0].link} />
+				<Card image={foods} title={data[1].title} techFront={data[1].techFront} techBack={data[1].techBack} text={data[1].subtitle} link={data[1].link} />
+				<Card image={findahome} title={data[2].title} techFront={data[2].techFront} techBack={data[2].techBack} text={data[2].subtitle} link={data[2].link} />
 			</FlexContainer>
 		</FlexContainer>
 	)
