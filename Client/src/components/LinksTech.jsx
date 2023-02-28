0254942a6e5a3c895fed9607e1d83fc7c4fc8cee
HiUlisesOk <uesquivel95@gmail.com>
2023-02-28 20:12:23 -0300

    :sparkles: Adding links to techlink

--- Client/src/components/LinksTech.jsx
+++ Client/src/components/LinksTech.jsx
@@ -4,6 +4,7 @@ import linkedin from '../images/Assets/Linkedin.png'
 import Button from '@mui/material/Button';
 import DownloadIcon from '@mui/icons-material/Download';
 import { FlexContainer, LinksContainer } from './styles'
+import { Link } from 'react-router-dom';
 const LinksTech = () => {
 	return (<FlexContainer justify="flex-end">
 
@@ -11,10 +12,10 @@ const LinksTech = () => {
 
 			<LinksContainer justify="flex-start">
 
-				<img src={linkedin} alt="linkedIn Button" />
-				<img src={github} alt="Github Button" />
+				<Link to="https://github.com/HiUlisesOk"><img src={linkedin} alt="linkedIn Button" /></Link>
+				<Link to="https://www.linkedin.com/in/ulises-esquivel/"><img src={github} alt="Github Button" /></Link>
 
-				<Button variant="contained" color="primary" sx={{ height: "1.5rem", zIndex: 999 }} endIcon={<DownloadIcon />}>Get Resume</Button>
+				<Link to="https://docs.google.com/document/d/13BTFdcn-H6I5OckC_nNb3JMAh4k_mr4I0CDLu9KiX5I/edit?usp=sharing"><Button variant="contained" color="primary" sx={{ height: "1.5rem", zIndex: 999 }} endIcon={<DownloadIcon />}>Get Resume</Button></Link>
 
 			</LinksContainer>
 
